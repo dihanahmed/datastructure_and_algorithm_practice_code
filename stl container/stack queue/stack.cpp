ae5e8187cb1272f2014f79dde9d2716265a07b2a
hasnaine ahmed dihan <dihanahmed205@gmail.com>
2021-11-23 14:01:34 +0600

    stack queue

---
+++ stl container/stack queue/stack.cpp
@@ -0,0 +1,22 @@
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  stack<int> S;
+  S.push(5);
+  S.push(6);
+  S.push(7);
+   cout<<S.size()<<" "<<S.top()<<endl;
+   S.pop();
+   cout<<S.size()<<" "<<S.top()<<endl;
+
+}
+
+
+
